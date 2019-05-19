cdcb784b8b224c2d25e08b00f1a6f0a1389edabe
AsdoshDev <karthikdev27@gmail.com>
2019-05-19 19:22:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,41 @@ function sumAll(arr) {
 // sum of a range of numbers with arithmetic progression :
 
 var sum = (lastNum - firstNum + 1) * (firstNum + lastNum) / 2;
+
+
+
+
+// compare two arrays and return unique items  - MY SOLUTION
+
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  for(let i=0;i<arr2.length;i++){
+    debugger;
+    if(!arr1.includes(arr2[i]))
+      newArr.push(arr2[i]);
+  }
+  for(let i=0;i<arr1.length;i++){
+    if(!arr2.includes(arr1[i]))
+      newArr.push(arr1[i]);
+  }
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+// OTHER SOLUTIONS : merge the arrays and then filter ones thats not present in other.
+
+ function diffArray(arr1, arr2) {
+      return arr1
+        .concat(arr2)
+        .filter(
+            item => !arr1.includes(item) || !arr2.includes(item)
+        )
+    }
+
+
+
+
+
+
