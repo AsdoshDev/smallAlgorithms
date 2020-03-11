3b617365a11d646a9da0d55effc275b2b345b504
AsdoshDev <karthikdev27@gmail.com>
2020-03-11 08:56:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// find the odd number in the bunch. even/odd is the oddness. 
+function iqTest(numbers){
+  var nums = numbers.split(" ").map(x => x % 2);  
+  var sum = nums.reduce((a,b) => a + b);  
+  var target = sum > 1 ? 0 : 1;
+  
+  return nums.indexOf(target) + 1;
+}
+
 // create a string in a certain format with input as an array
 function createPhoneNumber(numbers){
   var format = "(xxx) xxx-xxxx";
