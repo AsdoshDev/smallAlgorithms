5dba99fa90159d31376a4dc8f8fa0da7f69d9fb9
AsdoshDev <karthikdev27@gmail.com>
2020-03-13 09:11:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// find the roman equivalent of a number
+function solution(number){
+  // convert the number to a roman numeral
+var  roman = {M:1000,CM:900, D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1 }
+
+var ans = '';
+while(number>0){
+    console.log(number +  " > 0");
+    for(a in roman){ 
+       console.log("ROMAN " + a);
+        if(roman[a]<=number){ 
+          console.log(roman[a] +  " <= " + number);
+          ans += a; 
+          console.log("ans " + ans);
+          number-=roman[a]; break;
+          console.log("number " + number);
+          }
+    }
+}
+return ans;
+}
+
+
+
 // find the odd number in the bunch. even/odd is the oddness. 
 function iqTest(numbers){
   var nums = numbers.split(" ").map(x => x % 2);  
