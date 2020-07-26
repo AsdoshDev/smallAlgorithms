161856de1e4e9122dd2ce175ab19a579294516b0
AsdoshDev <karthikdev27@gmail.com>
2020-07-26 12:38:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// ACTUAL ANSWER FOR MISSING POSITIVE NUMBER
+var firstMissingPositive = function(nums) {
+    let i = 0;
+    while (i < nums.length) {
+        if (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] !== nums[i]) {
+            [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+        } else {
+            i++;
+        }
+    }
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] !== i + 1) return i + 1;
+    }
+    return i + 1;
+};
+
+
 //find the first missing positive integer.
 
 /**
